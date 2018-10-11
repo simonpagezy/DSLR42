df280d7c3b41face499011e982bd0a273a0a2225
unknown <simon.pagezy@hec.edu>
2018-10-11 12:17:22 +0200

    added comments to explain the funcions

--- histogram.py
+++ histogram.py
@@ -12,17 +12,38 @@ import numpy as np
 import sys
 
 def preprocess(dataset):
+    """
+    Preprocess the training dataset: keep the numeric columns and scale
+    features.
+    Inputs:
+        - dataset (pd.dataframe) training dataset of Harry Potter
+    """
     cols = numeric_features(dataset)
     for col in cols:
         dataset[col] = (dataset[col] - dataset[col].mean()) / dataset[col].std()
     return dataset[cols]
 
-def get_grades(dataset, prep_dataset, house, topics):
-    df = prep_dataset[dataset["Hogwarts House"] == house][topics]
+def get_grades(dataset, prep_dataset, house, topic):
+    """
+    Get a datafram with the grades of the student from a specific house in a 
+    specific topic.
+    Inputs:
+        - dataset (pd.dataframe) training dataset of Harry Potter
+        - prep_dataset (pd.dataframe) preprocessed dataset of Harry Potter
+        - house (str) name of the House you want to focus on
+        - topic (str) name of the Topic you want to focus on
+    """
+    df = prep_dataset[dataset["Hogwarts House"] == house][topic]
     df.dropna(inplace=True)
     return df
 
 def plot_hist(dataset, prep_dataset):
+    """
+    Plot one histogram per topic to see the repartition of grades of the houses
+    Inputs:
+        - dataset(pd.dataframe) training dataset of HP
+        - prep_dataset(pd.dataframe) preprocessed dataset of HP
+    """
     for col in prep_dataset.columns:
         plt.figure()
         plt.hist(get_grades(dataset, prep_dataset, "Gryffindor", col), bins=25, alpha=0.5, label = 'Gry', color = 'r')
