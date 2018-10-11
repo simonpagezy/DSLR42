1a0c7c171be57055a60b284e929c8487411c28c0
unknown <simon.pagezy@hec.edu>
2018-10-11 12:11:48 +0200

    function to plot running the pscirpt from the console

--- histogram.py
+++ histogram.py
@@ -6,13 +6,10 @@ Created on Mon Oct  8 19:05:33 2018
 """
 
 import pandas as pd
-from describe import manual_describe, numeric_features
+from describe import numeric_features
 import matplotlib.pyplot as plt
 import numpy as np
-
-dataset = pd.read_csv('resources/dataset_train.csv', index_col = "Index")
-result_df = manual_describe(dataset)
-
+import sys
 
 def preprocess(dataset):
     cols = numeric_features(dataset)
@@ -20,47 +17,25 @@ def preprocess(dataset):
         dataset[col] = (dataset[col] - dataset[col].mean()) / dataset[col].std()
     return dataset[cols]
 
-
-def get_grades(prep_dataset, house, topics):
-    df = prep_dataset[prep_dataset["Hogwarts House"] == house][topics]
+def get_grades(dataset, prep_dataset, house, topics):
+    df = prep_dataset[dataset["Hogwarts House"] == house][topics]
+    df.dropna(inplace=True)
     return df
 
-def plot_hist(dataset):
-    for col in numeric_features(dataset):
-        plt.hist(get_grades(dataset, "Gryffindor", col), bins=25, alpha=0.5, label = 'Gry', color = 'y')
+def plot_hist(dataset, prep_dataset):
+    for col in prep_dataset.columns:
+        plt.figure()
+        plt.hist(get_grades(dataset, prep_dataset, "Gryffindor", col), bins=25, alpha=0.5, label = 'Gry', color = 'r')
+        plt.hist(get_grades(dataset, prep_dataset, "Ravenclaw", col), bins=25, alpha=0.5, label = 'Rav', color = 'b')
+        plt.hist(get_grades(dataset, prep_dataset, "Slytherin", col), bins=25, alpha=0.5, label = 'Sly', color = 'g')
+        plt.hist(get_grades(dataset, prep_dataset, "Hufflepuff", col), bins=25, alpha=0.5, label = 'Huf', color = 'y')
         plt.legend(loc = 'upper right')
         plt.title(col)
         plt.show()
     
-        
-        
-        
-
-
-
-
-
-#for col in result_df.columns:
-#     grouped = dataset.groupby("Hogwarts House", axis = 0)[col].mean()
-#
-#
-#
-#def create_df(dataset):
-#    """
-#    Create a matrix to stock the needed result. 
-#    """
-#    cols = numeric_features(dataset)
-#    info = dataset["Hogwarts House"].unique()
-#    result_df = pd.DataFrame(np.zeros(shape=(len(info), len(cols))), index = info, columns = cols)
-#    return result_df
-#
-#def fill_df(dataset, result_df):
-#    """
-#    Fill in df with the needed results (mean per topic grouped by house)
-#    Normalize the results
-#    """
-#    for col in result_df.columns:
-#        grouped = dataset.groupby("Hogwarts House", axis=0)[col].mean()
-#        center_val = (grouped.values - np.mean(grouped.values))/np.std(grouped.values)
-#        result_df[col] = center_val
-#    return result_df
+if __name__ == "__main__":
+    dataset = pd.read_csv(sys.argv[1], index_col = "Index")
+    prep_dataset = preprocess(dataset)
+    plot_hist(dataset, prep_dataset)
+    
+    
